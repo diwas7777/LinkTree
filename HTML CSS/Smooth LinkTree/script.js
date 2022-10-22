7ba0994f621f737c590c87f07f16bc2d9308c0d7
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2022-10-22 11:13:24 +0530

    Create script.js

---
+++ HTML CSS/Smooth LinkTree/script.js
@@ -0,0 +1,20 @@
+fetch("data.json")
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (products) {
+    let placeholder = document.querySelector("#links");
+    let out = "";
+    for (let product of products) {
+      if (`${product.doYouWantToShowThis}` == "yes") {
+        out += `
+        <a class="link" href="${product.link}" target="_blank">
+        <i class="${product.icons}" style="padding: 10px"></i> 
+        ${product.contentToShow}
+        </a>
+        `;
+      }
+    }
+    placeholder.innerHTML = out;
+  });
